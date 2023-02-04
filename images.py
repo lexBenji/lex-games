b7ea528ea6e31571b5b66a0e17e4c585f222c694
lexBenji <discordo12341@outlook.com>
2023-02-04 18:18:02 +0700

    changed genration process

--- images.py
+++ images.py
@@ -6,7 +6,7 @@ def generate():
     z = ''
     for i in range(6):
         for j in range(8):
-            z += str(random.randint(1,5))
+            z += str(random.randint(0,4))
         generated.append(z)
         z = ''
     writeln('+----------+')
@@ -14,17 +14,15 @@ def generate():
         write('| ')
         for j in i:
             if j == '0':
-                write(' ')
+                write('.')
             elif j == '1':
                 write('-')
             elif j == '2':
                 write('~')
             elif j == '3':
-                write('%')
-            elif j == '4':
                 write('@')
-            elif j == '5':
-                write('#')
+            elif j == '4':
+                write('%')
         write(' |')
         writeln('')
     writeln('+----------+')
--- index.py
+++ index.py
@@ -2,40 +2,38 @@ import random
 import images
 from stuff import *
 
+print('Games: rg,rps')
+
 while True:
-    writeln('''
-Games: rg,rps
-Type "exit" to exit.
-''')
     game = input('Choose a game: ').lower()
     if game == 'rg':
         images.generate()
     elif game == 'rps':
         mc = random.choice(['rock','paper','scissors'])
         rps = input('Choose [r,p,s] ').lower()
+        writeln(f'I chose {mc}')
         if rps == 'r' or rps == 'rock':
-            writeln(f'I chose {mc}')
             if mc == 'rock':
                 writeln('tie')
             elif mc == 'paper':
-                writeln('i lost')
-            elif mc == 'scissors':
                 writeln('you lost')
+            elif mc == 'scissors':
+                writeln('i lost')
         elif rps == 'paper' or rps == 'p':
-            writeln(f'I chose {mc}')
             if mc == 'rock':
-                writeln('you lost')
+                writeln('i lost')
             elif mc == 'paper':
                 writeln('tie')
             elif mc == 'scissors':
-                writeln('i lost')
+                writeln('you lost')
         elif rps == 's' or rps == 'scissors':
-            writeln(f'I chose {mc}')
             if mc == 'rock':
                 writeln('you lost')
             elif mc == 'paper':
                 writeln('i lost')
             elif mc == 'scissors':
                 writeln('tie')
-    elif game == 'exit':
+    elif game == 'games' or game == '?' or game == 'h' or game == 'help':
+        print('Games: rg,rps')
+    elif game == 'exit' or game == 'x':
         break
